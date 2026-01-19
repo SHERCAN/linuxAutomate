97924c5581c0b1514eb68b26ab364fe7aec158ce
SHERCAN <90333070+SHERCAN@users.noreply.github.com>
2026-01-19 12:44:58 -0500

    Add Docker installation to install.sh
    Added Docker installation steps and user group configuration.

--- install.sh
+++ install.sh
@@ -58,6 +58,22 @@ sudo apt install -y dkms libdrm-dev
 # Nota: El driver oficial suele requerir descargar un .run de la web de Synaptics.
 # Este comando prepara el entorno para que el instalador no falle.
 
+#Instalacion cpmpleta de Docker 
+sudo apt update && sudo apt install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Crear el grupo docker y añadir tu usuario
+sudo usermod -aG docker $USER
+newgrp docker
+
 # Limpieza
 sudo apt autoremove -y
 echo "Instalación completada. Es recomendable REINICIAR para aplicar cambios en KVM y Drivers."
